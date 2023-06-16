93a1f89e2f179c881c14609280b09667c9af3294
denis-cohen <denis.cohen.132@gmail.com>
2023-06-16 15:20:48 +0200

    Fixed special case in compute_ce_qois (array with third dimension == 1). Added y_check to run_mavcl.

--- R/compute_ce_qois.R
+++ R/compute_ce_qois.R
@@ -48,11 +48,13 @@ compute_ce_qois <- function(ce_obj,
     ((ce_obj[, gain_vec, , drop = FALSE] %>% apply(c(1, 3), sum)) / denom)
 
   balance <-
-    (((ce_obj[, gain_vec, , drop = FALSE] - ce_obj[, loss_vec,]) %>%
+    (((ce_obj[, gain_vec, , drop = FALSE] -
+         ce_obj[, loss_vec, drop = FALSE]) %>%
         apply(c(1, 3), sum)) / denom)
 
   volume <-
-    (((ce_obj[, gain_vec, , drop = FALSE] + ce_obj[, loss_vec,]) %>%
+    (((ce_obj[, gain_vec, , drop = FALSE] +
+         ce_obj[, loss_vec, drop = FALSE]) %>%
         apply(c(1, 3), sum)) / denom)
 
   retention <-
